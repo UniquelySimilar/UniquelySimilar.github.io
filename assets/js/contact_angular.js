0c50f34d6feff72d6ee4b6dfaefe9fab74717a76
Tim <uniquelysimilar@yahoo.com>
2014-12-27 22:47:11 -0700

    disable submit button until response

--- assets/js/contact_angular.js
+++ assets/js/contact_angular.js
@@ -1,21 +1,25 @@
 var contactApp = angular.module("contactApp", []);
 
-contactApp.controller('ContactController', function($scope, $http) {
+contactApp.controller('ContactController', function($scope, $http, $timeout) {
 	$scope.showDebug = false;
 	contactServiceURL = "http://contact-service-timc.herokuapp.com/api/contacts"
 //	contactServiceURL = "http://contactservice.localhost/api/contacts"
 
 	$scope.processContact = function() {
+		$scope.inProcess = true;
+
 		// Simple POST request example (passing data) :
 		$http.post(contactServiceURL, $scope.contact).
 			success(function(data, status, headers, config) {
 				// this callback will be called asynchronously when the response is available
 				$scope.status = status;
+				$scope.success = true;
 				$scope.newContactName = $scope.contact.name;
 				//console.log("New contact name: " + $scope.newContactName);
-				$scope.contact = {};
-				$scope.success = true;
-				$scope.errors = "";
+				$scope.contact = null;
+				$scope.errors = null;
+
+				$scope.inProcess = false;
 			}).
 			error(function(data, status, headers, config) {
 				// called asynchronously if an error occurs or server returns response with an error status.
@@ -26,6 +30,10 @@ contactApp.controller('ContactController', function($scope, $http) {
 					console.log("ERROR: 'exception' property found in response, indicating server caught and returned an exception.");
 					console.log("Exception message: " + $scope.errors.exception);
 				}
+
+				$scope.inProcess = false;
 			});
+			
 	};
+
 });
